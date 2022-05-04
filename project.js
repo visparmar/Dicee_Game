1be92bfc502618ef5ca5c9574fe5372be23d3280
Vikas Parmar <36152918+visparmar@users.noreply.github.com>
2022-05-04 19:20:37 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,13 +1,11 @@
 //player 1.....
 var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomimage1="dice"+randomNumber1+".png";
-var randomSource1="images/"+randomimage1;
 var temp1=document.querySelectorAll("img")[0];
-temp1.setAttribute("src",randomSource1);
+temp1.setAttribute("src",randomimage1);
 
 //player 2......
 var randomNumber2=Math.floor(Math.random()*6)+1;
 var randomimage2="dice"+randomNumber2+".png";
-var randomSource2="images/"+randomimage2;
 var temp2=document.querySelectorAll("img")[1];
-temp2.setAttribute("src",randomSource2);
+temp2.setAttribute("src",randomimage2);
